47396fba2153dd8c227f80993fbfb138e83cdddd
ElielsonU <elielson7756@gmail.com>
2023-03-19 23:42:49 -0300

    cart post entry point created!

--- cart/models.py
+++ cart/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Cart(models.Model):
     name = models.CharField(max_length=50)
-    created_id = models.DateField()
+    created_in = models.DateField()
     
     def __str__(self):
         return self.name
--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
+import datetime
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "cart/index.html")
 
 def postCart(request):
-    
-    return
+    print(request.POST["name"])
+    cart = models.Cart()
+    cart.name = request.POST["name"]
+    cart.created_in = datetime.datetime.now()
+    cart.save()
+    return redirect("/")
+
+
