0934814a30270e0468769d6c0debc74e0edfd52f
ElielsonU <elielson7756@gmail.com>
2023-03-21 19:54:55 -0300

    delete function created!

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.index),
     path("<int:id>", views.carts),
     path("new", views.postCart),
+    path("newitem", views.postItem),
+    path("deleteitem/<int:id>", views.deleteItem),
+    path("deletecart/<int:id>", views.deleteCart),
 ]
--- cart/views.py
+++ cart/views.py
@@ -8,14 +8,11 @@ def index(request):
     return render(request, "cart/index.html", {"carts_list" : carts_list })
 
 def carts(request, id):
-    request.method
+
     cart = models.Cart.objects.get(id=id)
+
     items_list = models.Item.objects.filter(cart_id=id)
-    
-    context = {
-        "cart": cart,
-        "items_list": items_list,
-    }
+    context = { "cart": cart, "items_list": items_list, }        
 
     return render(request, "cart/carts.html", context)
 
@@ -25,6 +22,26 @@ def postCart(request):
     cart.name = request.POST["name"]
     cart.created_in = datetime.datetime.now()
     cart.save()
-    return redirect("/")
+    return redirect("/cart/%s"%cart.id)
 
+def deleteCart(request, id):
+    cart = models.Cart.objects.get(id=id)
+    cart.delete()
+    return redirect("/cart")
+
+def postItem(request):
+    cart = models.Cart.objects.get(id=request.POST["cart_id"])
+    if request.method == "POST":
+        item = models.Item()
+        item.name = request.POST["name"]
+        item.value = request.POST["value"]
+        item.cart_id = cart
+        item.save()
+    return redirect("/cart/%s"%request.POST["cart_id"])
+
+def deleteItem(request, id):
+    item = models.Item.objects.get(id=id)
+    cart_id = item.cart_id.id
+    item.delete()
+    return redirect("/cart/%s"%cart_id)
 
